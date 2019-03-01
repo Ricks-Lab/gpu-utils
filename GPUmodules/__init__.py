418e7591adea361233961fc8a1a0d68d3be3f904
Rick <ricks.homelab@gmail.com>
2019-03-01 09:02:30 +0800

    changed GPUmodules.py to GPUmodule.py for consistency

--- GPUmodules/__init__.py
+++ GPUmodules/__init__.py
@@ -14,7 +14,7 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from GPUmodules import GPUmodules
+from GPUmodules import GPUmodule
 from GPUmodules import env
 from GPUmodules import PCImodule
 
