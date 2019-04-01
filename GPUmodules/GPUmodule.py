b00c8e52ea4240d0a50b9ea16faae4b31ebdd9b8
Rick <ricks.homelab@gmail.com>
2019-04-01 18:21:40 +0800

    compare curve pts to pt specific ranges

--- GPUmodules/GPUmodule.py
+++ GPUmodules/GPUmodule.py
@@ -291,12 +291,12 @@ class GPU_ITEM:
         return(False)
 
     def is_valid_vddc_curve_pts(self, curve_pts):
-        sclk_min = int(re.sub(r'[a-z,A-Z]*', '', str(self.vddc_curve_range["0"]["SCLK"][0])))
-        sclk_max = int(re.sub(r'[a-z,A-Z]*', '', str(self.vddc_curve_range["0"]["SCLK"][1])))
+        sclk_min = int(re.sub(r'[a-z,A-Z]*', '', str(self.vddc_curve_range[str(curve_pts[0])]["SCLK"][0])))
+        sclk_max = int(re.sub(r'[a-z,A-Z]*', '', str(self.vddc_curve_range[str(curve_pts[0])]["SCLK"][1])))
         if curve_pts[1] < sclk_min or curve_pts[1] > sclk_max:
             return(False)
-        vddc_min = int(re.sub(r'[a-z,A-Z]*', '', str(self.vddc_curve_range["0"]["VOLT"][0])))
-        vddc_max = int(re.sub(r'[a-z,A-Z]*', '', str(self.vddc_curve_range["0"]["VOLT"][1])))
+        vddc_min = int(re.sub(r'[a-z,A-Z]*', '', str(self.vddc_curve_range[str(curve_pts[0])]["VOLT"][0])))
+        vddc_max = int(re.sub(r'[a-z,A-Z]*', '', str(self.vddc_curve_range[str(curve_pts[0])]["VOLT"][1])))
         if curve_pts[2] < vddc_min or curve_pts[2] > vddc_max:
             return(False)
         return(True)
