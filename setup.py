25f258d7b16f80f04580e99668fe9871089d11fd
Rick <ricks.homelab@gmail.com>
2019-09-11 19:41:03 +0800

    style updates

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 from distutils.core import setup
 
 setup(name='amdgpu-utils',
-      version='2.5.2',
+      version='2.6.0',
       description='Ricks-Lab AMD GPU Utilities',
       author='RueiKe',
       author_email='rueikes.homelab@gmail.com',
       url='https://github.com/Ricks-Lab/amdgpu-utils',
       packages=['GPUmodules'],
-      scripts=['amdgpu-chk','amdgpu-ls','amdgpu-monitor','amdgpu-pac','amdgpu-pciid','amdgpu-plot'],
-      licene="GPL-3",
-      package_data={'GPUmodules':['amd_pci_id.txt']}
-)
+      scripts=['amdgpu-chk', 'amdgpu-ls', 'amdgpu-monitor', 'amdgpu-pac', 'amdgpu-pciid', 'amdgpu-plot'],
+      license="GPL-3",
+      python_requires='>=3.6',
+      package_data={'GPUmodules': ['amd_pci_id.txt']}
+      )
