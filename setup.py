41047abca2e4d890cc546d3d393a4312018fde57
Rick <ricks.homelab@gmail.com>
2019-10-10 10:09:09 +0800

    include requirements

--- setup.py
+++ setup.py
@@ -10,5 +10,16 @@ setup(name='amdgpu-utils',
       packages=['GPUmodules'],
       scripts=['amdgpu-chk', 'amdgpu-ls', 'amdgpu-monitor', 'amdgpu-pac', 'amdgpu-pciid', 'amdgpu-plot'],
       license="GPL-3",
-      package_data={'GPUmodules': ['amd_pci_id.txt']}
+      package_data={'GPUmodules': ['amd_pci_id.txt']},
+      install_requires=['cycler==0.10.0',
+                        'kiwisolver==1.1.0',
+                        'matplotlib==3.0.3',
+                        'numpy==1.16.3',
+                        'pandas==0.24.2',
+                        'pyparsing==2.4.0',
+                        'python-dateutil==2.8.0',
+                        'pytz==2019.1',
+                        'ruamel.yaml==0.16.5',
+                        'ruamel.yaml.clib==0.1.2',
+                        'six==1.12.0']
       )
