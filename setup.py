fa8d3d1b82512e1d5d4d17b4e9da6aa7ce3f25f9
Rick <ricks.homelab@gmail.com>
2020-06-26 09:23:04 +0800

    deprecation release

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ if sys.version_info < (3, 6):
     print('ricks-amdgpu-utils requires at least Python 3.6.')
     sys.exit(1)
 
-VERSION = '3.3.10'
+VERSION = '3.3.11'
 
 setup(name='ricks-amdgpu-utils',
       version=VERSION,
-      description='Ricks-Lab AMD GPU Utilities',
-      long_description='A set of utilities for monitoring AMD GPU performance and modifying control settings.',
+      description='Ricks AMD GPU Utilities (Deprecated)',
+      long_description=('This release of GPU Utilities is replaced by rickslab-gpu-utils, which now supports.'
+                        'additional vendors GPUs.  The amdgpu-util version will no longer be updated, as'
+                        'development efforts will be focused on rickslab-gpu-utils'),
       author='RueiKe',
       platforms='posix',
       author_email='rueikes.homelab@gmail.com',
@@ -22,7 +24,7 @@ setup(name='ricks-amdgpu-utils',
       scripts=['amdgpu-chk', 'amdgpu-ls', 'amdgpu-monitor', 'amdgpu-pac', 'amdgpu-plot'],
       license='GPL-3',
       python_requires='>=3.6',
-      classifiers=['Development Status :: 5 - Production/Stable',
+      classifiers=['Development Status :: 6 - Mature',
                    'Operating System :: POSIX',
                    'Natural Language :: English',
                    'Programming Language :: Python :: 3',
