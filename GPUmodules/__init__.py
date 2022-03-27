b5d0910f0c7aab2d58450806d369ad90b368c1ea
Rick <ricks.homelab@gmail.com>
2022-03-27 16:51:53 +0800

    set development version

--- GPUmodules/__init__.py
+++ GPUmodules/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '3.6.2'
-__status__ = 'Development Status :: 5 - Production/Stable'
-#__status__ = 'Development Status :: 4 - Beta'
+__version__ = '3.6.3'
+#__status__ = 'Development Status :: 5 - Production/Stable'
+__status__ = 'Development Status :: 4 - Beta'
