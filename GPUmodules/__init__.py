88f1d80ab9e0b43708d82b73d81a7fda42607b38
Rick <ricks.homelab@gmail.com>
2022-06-09 12:45:54 +0800

    prep for development

--- GPUmodules/__init__.py
+++ GPUmodules/__init__.py
@@ -1,6 +1,6 @@
-__version__ = '3.7.6'
-__status__ = 'Development Status :: 5 - Production/Stable'
-#__status__ = 'Development Status :: 4 - Beta'
+__version__ = '3.7.7'
+#__status__ = 'Development Status :: 5 - Production/Stable'
+__status__ = 'Development Status :: 4 - Beta'
 __author__ = 'RicksLab'
 __credits__ = ('Craig Echt - Testing, Debug, Verification, and Documentation',
                'Keith Myers - Testing, Debug, and Verification of NV Capability',
